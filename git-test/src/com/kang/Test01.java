639e8666057fc1d5ba82242f479025d37f3c4d9c
kangkang <824335342@qq.com>
2020-01-10 22:24:42 +0800

    第一次提交

---
+++ git-test/src/com/kang/Test01.java
@@ -0,0 +1,9 @@
+package com.kang;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+        System.out.println("hello,git!");
+        System.out.println("hello,git!");
+    }
+}
